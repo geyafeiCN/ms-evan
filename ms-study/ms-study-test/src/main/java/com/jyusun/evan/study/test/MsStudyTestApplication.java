4a9d677c2c121765db0dcd1ec7c07210b8b53888
孙建宇 <sunjianyu@ysstech.com>
2019-04-11 17:39:53 +0800

    提交测试项目

---
+++ ms-study/ms-study-test/src/main/java/com/jyusun/evan/study/test/MsStudyTestApplication.java
@@ -0,0 +1,13 @@
+package com.jyusun.evan.study.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsStudyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsStudyTestApplication.class, args);
+    }
+
+}
---
+++ ms-study/ms-study-test/src/main/java/com/jyusun/evan/study/test/Rest/RestTest.java
@@ -0,0 +1,10 @@
+package com.jyusun.evan.study.test.Rest;
+
+/**
+ * 作用描述：
+ *
+ * @author JyuSun at 2019/4/11 17:39
+ * @version 1.0.0
+ */
+public class RestTest {
+}
